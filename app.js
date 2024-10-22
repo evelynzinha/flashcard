52e4b693efe20aaea15e170f7678fa26d11cf725
evelynzinha <148580817+evelynzinha@users.noreply.github.com>
2024-10-22 16:13:48 -0300

    ,jilhrqukn2uhd

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ function criaCartao(categoria, pergunta, resposta) {
     <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
     </div>
+    <div class="cartao__conteudo__resposta">
+    <p> ${resposta}</p>
+    </div>
+    </div>
+    `
+
+    let respostaEstavisivel = false
+
+    function viraCartao 
